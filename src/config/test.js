fea0984c6111efb45107df86ae2c3dc52e6befc3
Rafael Prado <contato@rprado.design>
2023-09-01 13:17:00 -0300

    refactor: capture items from fandom table

---
+++ src/config/test.js
@@ -0,0 +1,34 @@
+const spinnies = require("./config/spinnies");
+const fs = require("fs");
+const messages = JSON.parse(fs.readFileSync("./src/config/messages.json"));
+const targets = JSON.parse(fs.readFileSync("./src/config/targets.json"));
+
+(async () => {
+  spinnies.add(messages.puppeteer.init.id, {
+    text: messages.puppeteer.init.text.add,
+  });
+  const browser = await puppeteer.launch({ headless: "new" });
+  const context = await browser.createIncognitoBrowserContext();
+  const page = await context.newPage();
+  spinnies.succeed(messages.puppeteer.init.id, {
+    text: messages.puppeteer.init.text.succeed,
+  });
+
+  spinnies.add(messages.access.id, {
+    text: messages.access.text.add,
+  });
+  await page.goto(targets.url);
+  await page.waitForSelector(targets.table);
+
+  spinnies.succeed(messages.access.id, {
+    text: messages.access.text.succeed,
+  });
+
+  spinnies.add(messages.puppeteer.close.id, {
+    text: messages.puppeteer.close.text.add,
+  });
+  await browser.close();
+  spinnies.succeed(messages.puppeteer.close.id, {
+    text: messages.puppeteer.close.text.succeed,
+  });
+})();
--- src/index.js
+++ src/index.js
@@ -1,35 +1,22 @@
 const puppeteer = require("puppeteer");
-const spinnies = require("./config/spinnies");
 const fs = require("fs");
-const messages = JSON.parse(fs.readFileSync("./src/config/messages.json"));
 const targets = JSON.parse(fs.readFileSync("./src/config/targets.json"));
 
 (async () => {
-  spinnies.add(messages.puppeteer.init.id, {
-    text: messages.puppeteer.init.text.add,
-  });
-  const browser = await puppeteer.launch({ headless: "new" });
-  const context = await browser.createIncognitoBrowserContext();
-  const page = await context.newPage();
-  spinnies.succeed(messages.puppeteer.init.id, {
-    text: messages.puppeteer.init.text.succeed,
-  });
-
-  spinnies.add(messages.access.id, {
-    text: messages.access.text.add,
-  });
+  const browser = await puppeteer.launch({ headless: false, devtools: true });
+  const page = await browser.newPage();
   await page.goto(targets.url);
-  await page.waitForSelector(targets.table);
+  await page.waitForSelector(".table-wide-inner table tr th");
 
-  spinnies.succeed(messages.access.id, {
-    text: messages.access.text.succeed,
+  await page.evaluate(() => {
+    const nodeList = document.querySelectorAll(".table-wide-inner table tr th");
+
+    let trArray = Array.from(nodeList);
+
+    console.log(trArray[3].innerText);
   });
 
-  spinnies.add(messages.puppeteer.close.id, {
-    text: messages.puppeteer.close.text.add,
-  });
-  await browser.close();
-  spinnies.succeed(messages.puppeteer.close.id, {
-    text: messages.puppeteer.close.text.succeed,
-  });
+  // console.log(nodeList);
+
+  // await browser.close();
 })();
